8aa830eceda08a2ea39f0ab04d1d9fe04331719d
i-akshat-jain <108737870+akshatdebtenforce@users.noreply.github.com>
2024-04-16 18:38:03 +0100

    finished crm project

--- website/models.py
+++ website/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
-class Teacher(models.Model):
-    name = models.CharField(max_length=80)
-    age = models.IntegerField()
+
+class Record(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	first_name = models.CharField(max_length=50)
+	last_name =  models.CharField(max_length=50)
+	email =  models.CharField(max_length=100)
+	phone = models.CharField(max_length=15)
+	address =  models.CharField(max_length=100)
+	city =  models.CharField(max_length=50)
+	state =  models.CharField(max_length=50)
+	zipcode =  models.CharField(max_length=20)
+
+	def __str__(self):
+		return(f"{self.first_name} {self.last_name}")
